e9f0d86ab69513275844c921fb60c4fa330ef478
Andrew McConachie <andrew@depht.com>
2021-01-13 14:17:18 +0000

    unique-sources looks to work

--- api/v1/unique-sources.php
+++ api/v1/unique-sources.php
@@ -1,13 +1,17 @@
 <?php
 /* Copyright Andrew McConachie <andrew@depht.com> 2021 */
+//error_reporting(E_ALL);
 
-require_once "lib.php";
+require_once "../../lib.php";
 header('Content-Type: application/json');
 
+if( !isset($_GET['letters']) || !isset($_GET['start_date']) || !isset($_GET['end_date'])){ http_response_code(400); }
 
-var_dump($_POST);
+$raw_metrics = get_metrics_by_date('unique-sources', $_GET['letters'], $_GET['start_date'], $_GET['end_date']);
+if( $raw_metrics === false){ http_response_code(400); }
 
+$output = json_encode($raw_metrics);
+if( $output === false){ http_response_code(500); }
 
-
-
+print($output);
 ?>
