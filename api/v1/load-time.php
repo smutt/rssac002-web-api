4a232a2bcb1c8e10fd35a9ee17b0c184b33a09c5
Andrew McConachie <andrew@depht.com>
2021-01-13 13:34:00 +0000

    rcode-volume looks to work

--- api/v1/load-time.php
+++ api/v1/load-time.php
@@ -5,15 +5,13 @@
 require_once "../../lib.php";
 header('Content-Type: application/json');
 
+if( !isset($_GET['letters']) || !isset($_GET['start_date']) || !isset($_GET['end_date'])){ http_response_code(400); }
+
 $raw_metrics = get_metrics_by_date('load-time', $_GET['letters'], $_GET['start_date'], $_GET['end_date']);
-if( $raw_metrics === false){
-  http_response_code(400);
-  exit(1);
-}
+if( $raw_metrics === false){ http_response_code(400); }
+
 $output = json_encode($raw_metrics);
-if( $output === false){
-  http_response_code(500);
-  exit(1);
-}
+if( $output === false){ http_response_code(500); }
+
 print($output);
 ?>
