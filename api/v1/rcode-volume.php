ece06d8800e4b0cb0965b79cbe1548fb4a374892
Andrew McConachie <andrew@depht.com>
2021-01-13 14:31:44 +0000

    better error handling

--- api/v1/rcode-volume.php
+++ api/v1/rcode-volume.php
@@ -5,13 +5,22 @@
 require_once "../../lib.php";
 header('Content-Type: application/json');
 
-if( !isset($_GET['letters']) || !isset($_GET['start_date']) || !isset($_GET['end_date'])){ http_response_code(400); }
+if( !isset($_GET['letters']) || !isset($_GET['start_date']) || !isset($_GET['end_date'])){
+  http_response_code(400);
+  exit(1);
+}
 
 $raw_metrics = get_metrics_by_date('rcode-volume', $_GET['letters'], $_GET['start_date'], $_GET['end_date']);
-if( $raw_metrics === false){ http_response_code(400); }
+if( $raw_metrics === false){
+  http_response_code(400);
+  exit(1);
+}
 
 $output = json_encode($raw_metrics);
-if( $output === false){ http_response_code(500); }
+if( $output === false){
+  http_response_code(500);
+  exit(1);
+}
 
 print($output);
 ?>
