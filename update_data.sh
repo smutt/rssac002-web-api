545ec9713e26c93cadb202b8bf96bb796f8ebda0
Andrew McConachie <andrew@depht.com>
2024-01-09 16:23:51 +0000

    Added update_data.sh just to have somewhere to store it.

---
+++ update_data.sh
@@ -0,0 +1,19 @@
+#!/usr/local/bin/bash
+
+BASEDIR=/var/www/htdocs/rssac002.depht.com
+
+echo "Retrieving RSSAC002 data"
+cd $BASEDIR/RSSAC002-data
+git pull
+
+echo "Retrieving rss instance data"
+cd $BASEDIR/instance-data
+/usr/local/bin/wget --no-host-directories --max-redirect=0 --retry-on-host-error --recursive --https-only --no-clobber --no-parent https://root-servers.org/archives/ 2>&1 |egrep -v '^$'|grep -v "already there"
+
+echo "Serializing RSSAC002 and instance data"
+cd $BASEDIR/rssac002-web-api
+./prep_data.php
+
+echo "Generating static HTML for charts"
+cd $BASEDIR/rssac002-charts
+./gen_html.php
