56a815c24192af70233eb83ccedcb81b6b064106
Andrew McConachie <andrew@depht.com>
2024-04-05 12:03:27 +0000

    Make work on linux and obsd

--- update_data.sh
+++ update_data.sh
@@ -1,7 +1,8 @@
 #!/usr/local/bin/bash
 
-BASEDIR=/var/www/htdocs/rssac002.depht.com
+BASEDIR=/home/smutt/rssac002.depht.com
 WGET=/usr/local/bin/wget
+OS=bsd #linux || bsd
 
 echo "Retrieving RSSAC002 data"
 cd $BASEDIR/RSSAC002-data
@@ -9,25 +10,30 @@ git pull
 
 echo "Retrieving RZM data"
 cd $BASEDIR/RZM
-$WGET --no-verbose --no-host-directories --max-redirect=0 --retry-on-host-error --recursive --https-only --no-clobber --no-parent --accept "*zone-size.yaml" https://a.root-servers.org/rssac-metrics/raw/$(date +"%Y")/$(date +"%m")/zone-size 2>&1
+$WGET --no-verbose --no-host-directories --max-redirect=0 --recursive --https-only --no-clobber --no-parent --accept "*zone-size.yaml" https://a.root-servers.org/rssac-metrics/raw/$(date +"%Y")/$(date +"%m")/zone-size 2>&1
 
-# Now get the same from two days previous
-$WGET --no-verbose --no-host-directories --max-redirect=0 --retry-on-host-error --recursive --https-only --no-clobber --no-parent --accept "*zone-size.yaml" https://a.root-servers.org/rssac-metrics/raw/$(date -r $(($(date +"%s") - 172800)) +"%Y")/$(date -r $(($(date +"%s") - 172800)) +"%m")/zone-size 2>&1
+# Now get the same from the month of two days previous
+if [ "$OS" = "linux" ]
+then
+        $WGET --no-verbose --no-host-directories --max-redirect=0 --recursive --https-only --no-clobber --no-parent --accept "*zone-size.yaml" https://a.root-servers.org/rssac-metrics/raw/$(date --date=@$(($(date +"%s") - 172800)) +"%Y")/$(date --date=@$(($(date +"%s") - 172800)) +"%m")/zone-size 2>&1
+else
+        $WGET --no-verbose --no-host-directories --max-redirect=0 --recursive --https-only --no-clobber --no-parent --accept "*zone-size.yaml" https://a.root-servers.org/rssac-metrics/raw/$(date -r $(($(date +"%s") - 172800)) +"%Y")/$(date -r $(($(date +"%s") - 172800)) +"%m")/zone-size 2>&1
+fi
 
 echo "Retrieving rss instance data"
 cd $BASEDIR/instance-data
 rm $BASEDIR/instance-data/archives/index.html
 if [ "$(date +%m)" -eq "01" ] # Delete index.html from last 2 months
 then
-    find $BASEDIR/instance-data/archives/$(echo "$(date +%Y)-1" | bc) -name "index.html" -delete
-    find $BASEDIR/instance-data/archives/$(date +%Y)/01 -name "index.html" -delete
-    find $BASEDIR/instance-data/archives/$(echo "$(date +%Y)-1" | bc)/12 -name "index.html" -delete
+        find $BASEDIR/instance-data/archives/$(echo "$(date +%Y)-1" | bc) -name "index.html" -delete
+        find $BASEDIR/instance-data/archives/$(date +%Y)/01 -name "index.html" -delete
+        find $BASEDIR/instance-data/archives/$(echo "$(date +%Y)-1" | bc)/12 -name "index.html" -delete
 else
-    find $BASEDIR/instance-data/archives/$(date +%Y) -name "index.html" -delete
-    find $BASEDIR/instance-data/archives/$(date +%Y)/$(date +%m) -name "index.html" -delete
-    find $BASEDIR/instance-data/archives/$(date +%Y)/$(printf '%02d' $(echo "$(date +%m)-1" | bc)) -name "index.html" -delete
+        find $BASEDIR/instance-data/archives/$(date +%Y) -name "index.html" -delete
+        find $BASEDIR/instance-data/archives/$(date +%Y)/$(date +%m) -name "index.html" -delete
+        find $BASEDIR/instance-data/archives/$(date +%Y)/$(printf '%02d' $(echo "$(date +%m)-1" | bc)) -name "index.html" -delete
 fi
-$WGET --no-verbose --no-host-directories --max-redirect=0 --retry-on-host-error --recursive --https-only --no-clobber --no-parent https://root-servers.org/archives/ 2>&1
+$WGET --no-verbose --no-host-directories --max-redirect=0 --recursive --https-only --no-clobber --no-parent https://root-servers.org/archives/ 2>&1
 
 echo "Serializing RSSAC002 and instance data"
 cd $BASEDIR/rssac002-web-api
